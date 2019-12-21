24eff806a02b98f2ef26c9dfcbb6fe52e45d979a
Ivan Pantic <ivan@pantas.net>
2019-12-22 00:01:34 +0100

    Some meta

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Emby Fixer
 
-Fix emby by editing its database
+Fix emby by editing its database.
+
+Primarily, make music directories actually have directory name as title, instead of some random shit.
+
+More info: https://emby.media/community/index.php?/topic/16984-view-libraries-as-they-are-stored-by-folders/page-4
+
+### Setup
+
+Create `.env` and fill in required envs from `settings.js`. Need to run as root, or do some fancy sudoer stuff.
--- src/settings.js
+++ src/settings.js
@@ -7,9 +7,24 @@ require("dotenv").config({
 });
 
 module.exports = {
+  /**
+   * info or debug
+   */
   LOGGER_LEVEL: process.env.LOGGER_LEVEL || "info",
+
+  /**
+   * Path where enby db is stored. Try: /var/lib/emby/data/library.db
+   */
   EMBY_DB_PATH: process.env.EMBY_DB_PATH,
+
+  /**
+   * Root location where your music library is
+   */
   LIBRARY_LOCATION: process.env.LIBRARY_LOCATION,
+
+  /**
+   * Service name we will use to stop and restart emby server
+   */
   EMBY_SERVICE_NAME: process.env.EMBY_SERVICE_NAME || "emby-server.service"
 };
 
